d54ee66b42f09165be9c323a9a33c08f6aba640f
naomsa <omar@na2.com.br>
2022-01-07 14:39:28 -0300

    chore: changed folder structure

--- src/test/mocks/ERC721Mock.sol
+++ src/test/mocks/ERC721Mock.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "../../tokens/ERC721/ERC721.sol";
+import "../../tokens/ERC721.sol";
 
 contract ERC721Mock is ERC721("ERC721 Mock", "MOCK") {
   function mint(address to, uint256 tokenId) external {
