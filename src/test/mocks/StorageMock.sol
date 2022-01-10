78d3e923d420c72ac77348709f123b2376e3fd93
naomsa <omar@na2.com.br>
2022-01-10 15:44:50 -0300

    feat: added and tested Auth util

---
+++ src/test/mocks/StorageMock.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0 <0.9.0;
+
+import "../../utils/Auth.sol";
+
+contract StorageMock is Auth {
+  uint256 public number;
+
+  function store(uint256 num) public onlyAuthorized {
+    number = num;
+  }
+
+  function retrieve() public view returns (uint256) {
+    return number;
+  }
+}
---
+++ src/utils/Auth.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.0 <0.9.0;
+
+/**
+ * @title Auth
+ * @notice A simple but powerful authing system where the `owner` can authorize
+ * function calls to other addresses as well as control the contract by his own.
+ */
+abstract contract Auth {
+  /// @notice Contract's owner address.
+  address public owner;
+
+  /// @notice A mapping to retrieve if a call data was authed and is valid for the address.
+  mapping(address => mapping(bytes => bool)) private _isAuthorized;
+
+  /**
+   * @notice A modifier that requires the user to be the owner or authorization to call.
+   * After the call, the user loses it's authorization if he's not the owner.
+   */
+  modifier onlyAuthorized() {
+    require(isAuthorized(msg.sender, msg.data), "Auth: sender is not the owner or authorized to call");
+    _;
+    if (msg.sender != owner) _isAuthorized[msg.sender][msg.data] = false;
+  }
+
+  /// @notice A simple modifier just to check whether the sender is the owner.
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Auth: sender is not the owner");
+    _;
+  }
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function isAuthorized(address user_, bytes memory data_) public view returns (bool) {
+    return user_ == owner || _isAuthorized[user_][data_];
+  }
+
+  /// @notice Set the owner address to `owner_`.
+  function transferOwnership(address owner_) public onlyOwner {
+    require(owner_ != owner, "Auth: transfering ownership to current owner");
+    owner = owner_;
+  }
+
+  /// @notice Authorize a call with `data_` to the address `to_`.
+  function auth(address to_, bytes memory data_) public onlyOwner {
+    require(to_ != owner, "Auth: authorizing call to the owner");
+    require(!_isAuthorized[to_][data_], "Auth: authorized calls cannot be authed");
+    _isAuthorized[to_][data_] = true;
+  }
+
+  /// @notice Authorize a call with `data_` to the address `to_`.
+  function forbid(address to_, bytes memory data_) public onlyOwner {
+    require(_isAuthorized[to_][data_], "Auth: unauthorized calls cannot be forbidden");
+    delete _isAuthorized[to_][data_];
+  }
+}
