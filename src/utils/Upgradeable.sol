e7b6ace8d7941b30aff5278d60e7cb282e473d56
Omar <omar@na2.com.br>
2022-02-15 13:21:06 +0000

    feat: fixed upgradeable name and removed upgradeable tokens

--- src/utils/Upgradeable.sol
+++ src/utils/Upgradeable.sol
@@ -4,11 +4,11 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @title Upgradable
+ * @title Upgradeable
  * @author naomsa <https://twitter.com/naomsa666>
  * @notice Make your contract compatible with sol-temple's proxy.
  */
-contract Upgradable is Ownable {
+contract Upgradeable is Ownable {
   /// @notice See {OwnableProxy-_implementation}.
   address private _implementation;
 }
